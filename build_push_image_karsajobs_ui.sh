b790900c1709380f20bdd1b6efe72910247fd1d1
Rafi Ramadhan <rafiramadhanarr17@gmail.com>
2023-11-26 22:15:42 +0700

    Change env and sh

--- build_push_image_karsajobs_ui.sh
+++ build_push_image_karsajobs_ui.sh
@@ -7,8 +7,10 @@ docker images ls
 # Menyematkan tag pada image untuk digunakan saat push ke GitHub Container Registry
 docker tag karsajobs-ui:latest ghcr.io/rafrmdhn/karsajobs-ui:latest
 
+export CR_PAT=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
+
 # Melakukan login ke GitHub Container Registry menggunakan credentials yang disediakan
-echo $PASSWORD_PACKAGES | docker login ghcr.io --username rafrmdhn --password-stdin
+echo $CR_PAT | docker login ghcr.io --username rafrmdhn --password-stdin
 
 # Mengunggah image ke GitHub Container Registry
 docker push ghcr.io/rafrmdhn/karsajobs-ui:latest
